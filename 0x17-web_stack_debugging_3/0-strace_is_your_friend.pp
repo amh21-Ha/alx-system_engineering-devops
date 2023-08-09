e667fb95364209f65696dc5ab31b5a10e9ca36a7
Amha <amha.ha07@gmail.com>
2023-08-09 20:07:17 +0300

    rubby

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,10 @@
-# fixes Apache 500 error by fixing typo in wordpress
-exec { 'fix typo':
-  onlyif  => 'test -e /var/www/html/wp-settings.php',
-  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-}
+# A puppet manuscript to replace a line in a file on a server
+
+$file_to_edit = '/var/www/html/wp-settings.php'
+
+#replace line containing "phpp" with "php"
+
+exec { 'replace_line':
+  command => "sed -i 's/phpp/php/g' ${file_to_edit}",
+  path    => ['/bin','/usr/bin']
+}}
