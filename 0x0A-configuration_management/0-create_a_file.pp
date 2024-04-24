097bf7e91104bd9a8afe644dc8fb05bf629e4377
amha <amha.ha07@gmail.com>
2024-04-24 02:01:49 -0700

    puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,7 @@
-file{'school':
-path => '/tmp/school',
-ensure  => 'file',
-mode    => '0744',
-owner   => 'www-data',
-group   => 'www-data',
-content => 'I love Puppet',
+file { '/tmp/school':
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
